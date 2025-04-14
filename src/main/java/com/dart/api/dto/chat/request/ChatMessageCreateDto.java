d58831ec6ce8549c0f8f14cb822cca18d68b2ccb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-14 22:11:49 +0900

    refactor: 가독성 향상을 위한 줄바꿈

--- src/main/java/com/dart/api/dto/chat/request/ChatMessageCreateDto.java
+++ src/main/java/com/dart/api/dto/chat/request/ChatMessageCreateDto.java
@@ -1,14 +1,21 @@
 package com.dart.api.dto.chat.request;
 
+import java.time.LocalDateTime;
+
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record ChatMessageCreateDto(
+	String sender,
+
 	@Size(max = 50, message = "[❎ ERROR] 메시지 내용은 50자 이내여야 합니다.")
 	String content,
-	String sender,
-	String profileImageUrl,
-	boolean isAuthor
+
+	LocalDateTime createdAt,
+
+	boolean isAuthor,
+
+	String profileImageUrl
 ) {
 }
