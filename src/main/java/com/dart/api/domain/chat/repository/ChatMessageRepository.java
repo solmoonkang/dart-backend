b4823c7d74178b3a408c406d2532c15df3f73cc0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-15 00:18:26 +0900

    refactor: 불필요한 로직 제거

--- src/main/java/com/dart/api/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/dart/api/domain/chat/repository/ChatMessageRepository.java
@@ -1,10 +1,6 @@
 package com.dart.api.domain.chat.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.dart.api.domain.chat.entity.ChatMessage;
@@ -12,9 +8,6 @@ import com.dart.api.domain.chat.entity.ChatRoom;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-
-	@Query("SELECT cm FROM ChatMessage cm WHERE cm.chatRoom = :chatRoom ORDER BY cm.createdAt DESC")
-	Page<ChatMessage> findByChatRoomOrderByCreatedAtDesc(@Param("chatRoom") ChatRoom chatRoom, Pageable pageable);
 
 	void deleteByChatRoom(ChatRoom chatRoom);
 }
