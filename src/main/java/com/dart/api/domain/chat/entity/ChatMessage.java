5ad659975194ac9771aed36116f09f3120ba08ff
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-14 22:11:05 +0900

    refactor: BaseTimeEntity 상속 제거 및 createdAt 필드 추가

--- src/main/java/com/dart/api/domain/chat/entity/ChatMessage.java
+++ src/main/java/com/dart/api/domain/chat/entity/ChatMessage.java
@@ -1,5 +1,7 @@
 package com.dart.api.domain.chat.entity;
 
+import java.time.LocalDateTime;
+
 import com.dart.api.domain.member.entity.Member;
 import com.dart.api.dto.chat.request.ChatMessageCreateDto;
 import com.dart.api.dto.chat.request.ChatMessageSendDto;
@@ -24,7 +26,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Table(name = "tbl_chat_message")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ChatMessage extends BaseTimeEntity {
+public class ChatMessage {
 	@Id
 	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,6 +38,9 @@ public class ChatMessage extends BaseTimeEntity {
 	@Column(name = "is_author", nullable = false)
 	private boolean isAuthor;
 
+	@Column(name = "created_at", nullable = false)
+	private LocalDateTime createdAt;
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "chat_room_id")
 	private ChatRoom chatRoom;
@@ -45,9 +50,10 @@ public class ChatMessage extends BaseTimeEntity {
 	private Member member;
 
 	@Builder
-	private ChatMessage(String content, boolean isAuthor, ChatRoom chatRoom, Member member) {
+	private ChatMessage(String content, boolean isAuthor, LocalDateTime createdAt, ChatRoom chatRoom, Member member) {
 		this.content = content;
 		this.isAuthor = isAuthor;
+		this.createdAt = createdAt;
 		this.chatRoom = chatRoom;
 		this.member = member;
 	}
