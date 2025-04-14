ceb3935dc7c81c40dd7cd3b10a4574fb50246bbd
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-15 00:19:31 +0900

    feat: 캐시 만료 시간 상수 정의

--- src/main/java/com/dart/global/common/util/ChatConstant.java
+++ src/main/java/com/dart/global/common/util/ChatConstant.java
@@ -1,5 +1,7 @@
 package com.dart.global.common.util;
 
+import java.time.Duration;
+
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -12,7 +14,10 @@ public class ChatConstant {
 	public static final String ALLOWED_ORIGIN_PATTERN = "*";
 	public static final String CHAT_SESSION_USER = "authUser";
 	public static final String TOPIC_PREFIX = "/sub/ws/";
+	public static final String EXISTS_FLAG = "EXISTS";
+
 	public static final long CHAT_MESSAGE_EXPIRY_SECONDS = 60 * 60 * 48;
+	public static final Duration CACHE_EXPIRY_HOURS = Duration.ofHours(48);
 
 	public static final int MESSAGE_SIZE_LIMIT = 160 * 64 * 1024;
 	public static final int SEND_TIME_LIMIT = 100 * 10000;
