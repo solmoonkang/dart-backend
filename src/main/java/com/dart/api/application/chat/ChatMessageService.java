efd4df661ab40147ef4a657b53cb37a5cbc9e98a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-15 00:18:51 +0900

    refactor: 채팅 메시지 캐싱 처리 및 Redis 저장 로직 구현

--- src/main/java/com/dart/api/application/chat/ChatMessageService.java
+++ src/main/java/com/dart/api/application/chat/ChatMessageService.java
@@ -1,25 +1,13 @@
 package com.dart.api.application.chat;
 
-import static com.dart.global.common.util.ChatConstant.*;
+import static com.dart.global.error.model.ErrorCode.*;
 
-import java.util.AbstractMap;
-import java.util.List;
-
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.dart.api.domain.chat.entity.ChatMessage;
-import com.dart.api.domain.chat.entity.ChatRoom;
-import com.dart.api.domain.chat.repository.ChatMessageRepository;
-import com.dart.api.domain.chat.repository.ChatRedisRepository;
-import com.dart.api.domain.chat.repository.ChatRoomRepository;
-import com.dart.api.domain.member.entity.Member;
-import com.dart.api.domain.member.repository.MemberRepository;
+import com.dart.api.application.chat.batch.ChatCacheService;
+import com.dart.api.domain.chat.repository.ChatMessageRedisRepository;
 import com.dart.api.dto.chat.request.ChatMessageCreateDto;
-import com.dart.api.dto.chat.request.ChatMessageSendDto;
 import com.dart.global.error.exception.NotFoundException;
-import com.dart.global.error.model.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,66 +15,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ChatMessageService {
 
-	private final ChatRoomRepository chatRoomRepository;
-	private final MemberRepository memberRepository;
-	private final ChatRedisRepository chatRedisRepository;
-	private final ChatMessageRepository chatMessageRepository;
+	private final ChatMessageRedisRepository chatMessageRedisRepository;
+	private final ChatCacheService chatCacheService;
 
-	@Transactional
-	public void saveChatMessageToMySQL(Long chatRoomId, ChatMessageCreateDto chatMessageCreateDto) {
-		final ChatRoom chatRoom = getChatRoomById(chatRoomId);
-		final Member member = getMemberByNickname(chatMessageCreateDto.sender());
-		final ChatMessage chatMessage = ChatMessage.chatMessageFromCreateDto(chatRoom, member, chatMessageCreateDto);
-
-		chatMessageRepository.save(chatMessage);
+	public void saveChatMessage(Long chatRoomId, ChatMessageCreateDto chatMessageCreateDto) {
+		validateChatRoomExistsInCache(chatRoomId);
+		validateMemberExistsInCache(chatMessageCreateDto.sender());
+		chatMessageRedisRepository.saveChatMessage(chatRoomId, chatMessageCreateDto);
 	}
 
-	@Transactional
-	public void saveChatMessageToRedis(Long chatRoomId, ChatMessageCreateDto chatMessageCreateDto) {
-		final ChatRoom chatRoom = getChatRoomById(chatRoomId);
-		final Member member = getMemberByNickname(chatMessageCreateDto.sender());
-		final ChatMessage chatMessage = ChatMessage.chatMessageFromCreateDto(chatRoom, member, chatMessageCreateDto);
-
-		final ChatMessageSendDto chatMessageSendDto = chatMessage.toChatMessageSendDto(CHAT_MESSAGE_EXPIRY_SECONDS);
-		chatRedisRepository.saveChatMessage(chatMessageSendDto, member);
-		chatRedisRepository.saveBatchChatMessage(chatMessageSendDto, member);
-	}
-
-	@Scheduled(cron = CHAT_BATCH_SAVE_INTERVAL)
-	public void batchSaveMessages() {
-		List<Long> activeChatRoomIds = chatRedisRepository.getActiveChatRoomIds();
-
-		activeChatRoomIds.stream()
-			.map(chatRoomId -> {
-				List<ChatMessageCreateDto> messageCreateDtoList = chatRedisRepository.getAllBatchMessages(chatRoomId);
-				return new AbstractMap.SimpleEntry<>(chatRoomId, messageCreateDtoList);
-			})
-			.filter(entry -> !entry.getValue().isEmpty())
-			.forEach(entry -> saveMessagesIfNotEmpty(entry.getValue(), entry.getKey()));
-	}
-
-	private void saveMessagesIfNotEmpty(List<ChatMessageCreateDto> chatMessageCreateDtoList, Long chatRoomId) {
-		if (!chatMessageCreateDtoList.isEmpty()) {
-			final ChatRoom chatRoom = getChatRoomById(chatRoomId);
-			List<ChatMessage> chatMessageList = chatMessageCreateDtoList.stream()
-				.map(chatMessageCreateDto -> {
-					final Member member = getMemberByNickname(chatMessageCreateDto.sender());
-					return ChatMessage.chatMessageFromCreateDto(chatRoom, member, chatMessageCreateDto);
-				})
-				.toList();
-
-			chatMessageRepository.saveAll(chatMessageList);
-			chatRedisRepository.deleteBatchChatMessages(chatRoomId);
+	private void validateChatRoomExistsInCache(Long chatRoomId) {
+		if (!chatCacheService.isChatRoomCached(chatRoomId)) {
+			throw new NotFoundException(FAIL_CHAT_ROOM_NOT_FOUND);
 		}
 	}
 
-	private ChatRoom getChatRoomById(Long chatRoomId) {
-		return chatRoomRepository.findById(chatRoomId)
-			.orElseThrow(() -> new NotFoundException(ErrorCode.FAIL_CHAT_ROOM_NOT_FOUND));
-	}
-
-	private Member getMemberByNickname(String nickname) {
-		return memberRepository.findByNickname(nickname)
-			.orElseThrow(() -> new NotFoundException(ErrorCode.FAIL_MEMBER_NOT_FOUND));
+	private void validateMemberExistsInCache(String nickname) {
+		if (!chatCacheService.isMemberCached(nickname)) {
+			throw new NotFoundException(FAIL_MEMBER_NOT_FOUND);
+		}
 	}
 }
