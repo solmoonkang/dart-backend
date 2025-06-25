2d871672789fe1562f7754e481680a4e03a3d1f1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-06-25 17:45:42 +0900

    refactor: 검증 어노테이션 추가 및 정적 팩토리 생성 메서드 구현

--- src/main/java/com/dart/api/dto/chat/request/ChatMessageCreateDto.java
+++ src/main/java/com/dart/api/dto/chat/request/ChatMessageCreateDto.java
@@ -2,13 +2,16 @@ package com.dart.api.dto.chat.request;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record ChatMessageCreateDto(
+	@NotBlank(message = "[❎ ERROR] 사용자 닉네임을 입력해주세요.")
 	String sender,
 
+	@NotBlank(message = "[❎ ERROR] 메시지 내용을 입력해주세요.")
 	@Size(max = 50, message = "[❎ ERROR] 메시지 내용은 50자 이내여야 합니다.")
 	String content,
 
@@ -18,4 +21,10 @@ public record ChatMessageCreateDto(
 
 	String profileImageUrl
 ) {
+
+	public static ChatMessageCreateDto createChatMessageCreateDto(String sender, String content,
+		LocalDateTime createdAt, boolean isAuthor, String profileImageUrl) {
+
+		return new ChatMessageCreateDto(sender, content, createdAt, isAuthor, profileImageUrl);
+	}
 }
