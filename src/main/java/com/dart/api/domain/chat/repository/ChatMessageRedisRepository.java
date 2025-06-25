66fc73caf082b2cb9088ae5df7c53f483cbea778
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-06-25 17:46:00 +0900

    feat: 채팅 메시지 저장 및 캐싱 전략 처리 기능 구현

--- src/main/java/com/dart/api/domain/chat/repository/ChatMessageRedisRepository.java
+++ src/main/java/com/dart/api/domain/chat/repository/ChatMessageRedisRepository.java
@@ -5,19 +5,13 @@ import static com.dart.global.common.util.RedisConstant.*;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Repository;
 
 import com.dart.api.dto.chat.request.ChatMessageCreateDto;
-import com.dart.api.dto.chat.response.ChatMessageReadDto;
-import com.dart.api.infrastructure.batch.ChatMessageSerializer;
+import com.dart.api.infrastructure.redis.ValueRedisRepository;
 import com.dart.api.infrastructure.redis.ZSetRedisRepository;
+import com.dart.global.common.util.JsonConverter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,64 +20,50 @@ import lombok.RequiredArgsConstructor;
 public class ChatMessageRedisRepository {
 
 	private final ZSetRedisRepository zSetRedisRepository;
-	private final ChatMessageSerializer chatMessageSerializer;
+	private final ValueRedisRepository valueRedisRepository;
+	private final JsonConverter jsonConverter;
 
 	public void saveChatMessage(Long chatRoomId, ChatMessageCreateDto chatMessageCreateDto) {
-		LocalDateTime createdAt = Optional.ofNullable(chatMessageCreateDto.createdAt())
-			.orElse(LocalDateTime.now());
-
-		ChatMessageReadDto chatMessageReadDto = ChatMessageReadDto.fromChatMessageCreateDto(
-			chatRoomId, chatMessageCreateDto);
-		String messageJSON = chatMessageSerializer.serializeMessage(chatMessageReadDto);
-
-		double score = toEpochSecondScore(createdAt);
-
 		zSetRedisRepository.addElementWithExpiry(
-			generateChatMessageKey(chatRoomId), messageJSON, score, CHAT_MESSAGE_EXPIRY_SECONDS);
+			generateChatMessageKey(chatRoomId),
+			jsonConverter.toJson(chatMessageCreateDto),
+			convertToScore(chatMessageCreateDto.createdAt()),
+			CHAT_MESSAGE_EXPIRY_SECONDS);
 	}
 
-	public List<Long> getActiveChatRoomIds() {
-		Set<String> keys = zSetRedisRepository.getKeysByPattern(REDIS_CHAT_MESSAGE_PREFIX + "*");
-
-		if (keys == null || keys.isEmpty()) {
-			return Collections.emptyList();
-		}
-
-		return keys.stream()
-			.map(key -> key.replace(REDIS_CHAT_MESSAGE_PREFIX, ""))
-			.map(Long::valueOf)
-			.collect(Collectors.toList());
+	public void cacheChatRoom(Long chatRoomId) {
+		valueRedisRepository.saveValueWithExpiry(
+			generateChatRoomCacheKey(chatRoomId), "TURE", CACHE_EXPIRY_HOURS.getSeconds());
 	}
 
-	public List<ChatMessageReadDto> getMessagesAscFromChatRoom(
-		Long chatRoomId,
-		LocalDateTime archiveStartTime,
-		LocalDateTime archiveEndTime) {
-
-		double archiveStartTimeScore = toEpochSecondScore(archiveStartTime);
-		double toScore = toEpochSecondScore(archiveEndTime);
-
-		Set<String> messageJSONs = zSetRedisRepository.getRangeByScoreAsStringSet(
-			generateChatMessageKey(chatRoomId), archiveStartTimeScore, toScore);
-
-		if (messageJSONs == null || messageJSONs.isEmpty())
-			return Collections.emptyList();
-
-		return messageJSONs.stream()
-			.map(chatMessageSerializer::deserializeMessage)
-			.filter(Objects::nonNull)
-			.collect(Collectors.toList());
+	public void cacheMember(String nickname) {
+		valueRedisRepository.saveValueWithExpiry(
+			generateMemberCacheKey(nickname), "TRUE", CACHE_EXPIRY_HOURS.getSeconds());
 	}
 
-	public void deleteChatMessages(Long chatRoomId) {
-		zSetRedisRepository.deleteAllElements(generateChatMessageKey(chatRoomId));
+	public boolean isChatRoomCached(Long chatRoomId) {
+		return valueRedisRepository.isValueExists(generateChatRoomCacheKey(chatRoomId));
 	}
 
-	private double toEpochSecondScore(LocalDateTime createdAt) {
-		return createdAt.atZone(ZoneId.systemDefault()).toEpochSecond();
+	public boolean isMemberCached(String nickname) {
+		return valueRedisRepository.isValueExists(generateMemberCacheKey(nickname));
+	}
+
+	private long convertToScore(LocalDateTime createdAt) {
+		return createdAt.atZone(ZoneId.systemDefault())
+			.toInstant()
+			.toEpochMilli();
 	}
 
 	private String generateChatMessageKey(Long chatRoomId) {
 		return REDIS_CHAT_MESSAGE_PREFIX + chatRoomId;
+	}
+
+	private String generateChatRoomCacheKey(Long chatRoomId) {
+		return REDIS_CHAT_ROOM_CACHE_PREFIX + chatRoomId;
+	}
+
+	private String generateMemberCacheKey(String nickname) {
+		return REDIS_MEMBER_NICKNAME_CACHE_PREFIX + nickname;
 	}
 }
