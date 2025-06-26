ef5b1de9992e62c2212a8dc7164a045967ba8e92
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-06-26 19:09:35 +0900

    feat: 채팅메시지 배치 처리 스케줄러 로직 구현

--- src/main/java/com/dart/api/application/chat/message/ChatMessageService.java
+++ src/main/java/com/dart/api/application/chat/message/ChatMessageService.java
@@ -1,9 +1,15 @@
 package com.dart.api.application.chat.message;
 
+import static com.dart.global.common.util.RedisConstant.*;
 import static com.dart.global.error.model.ErrorCode.*;
 
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import com.dart.api.application.chat.message.batch.ChatMessageBatchService;
 import com.dart.api.domain.chat.repository.ChatMessageRedisRepository;
 import com.dart.api.dto.chat.request.ChatMessageCreateDto;
 import com.dart.global.error.exception.NotFoundException;
@@ -14,12 +20,28 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ChatMessageService {
 
+	private final ChatMessageBatchService chatMessageBatchService;
 	private final ChatMessageRedisRepository chatMessageRedisRepository;
 
 	public void saveChatMessage(Long chatRoomId, ChatMessageCreateDto chatMessageCreateDto) {
 		validateChatRoomExistsInCache(chatRoomId);
 		validateMemberExistsInCache(chatMessageCreateDto.sender());
 		chatMessageRedisRepository.saveChatMessage(chatRoomId, chatMessageCreateDto);
+	}
+
+	@Scheduled(cron = "0 */10 * * * *")
+	public void runBatchForAllRooms() {
+		final long maxScore = System.currentTimeMillis();
+
+		final Set<String> chatRoomKeys = chatMessageRedisRepository.findAllChatRoomKeysWithMessages();
+		final List<Long> chatRoomIds = chatRoomKeys.stream()
+			.map(key -> key.replace(REDIS_CHAT_MESSAGE_PREFIX, ""))
+			.map(Long::parseLong)
+			.toList();
+
+		for (Long chatRoomId : chatRoomIds) {
+			chatMessageBatchService.executeForChatRoom(chatRoomId, maxScore);
+		}
 	}
 
 	public void validateChatRoomExistsInCache(Long chatRoomId) {
