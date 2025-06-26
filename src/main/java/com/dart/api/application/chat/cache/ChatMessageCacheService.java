2b967ce781087daaeef5b24b53923720d47e1567
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-06-26 19:09:00 +0900

    refactor: 매개변수 DTO로 변경

--- src/main/java/com/dart/api/application/chat/cache/ChatMessageCacheService.java
+++ src/main/java/com/dart/api/application/chat/cache/ChatMessageCacheService.java
@@ -3,6 +3,8 @@ package com.dart.api.application.chat.cache;
 import org.springframework.stereotype.Service;
 
 import com.dart.api.domain.chat.repository.ChatMessageRedisRepository;
+import com.dart.api.dto.chat.request.cache.ChatRoomCacheDto;
+import com.dart.api.dto.chat.request.cache.MemberCacheDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,11 +14,11 @@ public class ChatMessageCacheService {
 
 	private final ChatMessageRedisRepository chatMessageRedisRepository;
 
-	public void cacheChatRoom(Long chatRoomId) {
-		chatMessageRedisRepository.cacheChatRoom(chatRoomId);
+	public void cacheChatRoom(ChatRoomCacheDto chatRoomCacheDto) {
+		chatMessageRedisRepository.cacheChatRoom(chatRoomCacheDto);
 	}
 
-	public void cacheMember(String nickname) {
-		chatMessageRedisRepository.cacheMember(nickname);
+	public void cacheMember(MemberCacheDto memberCacheDto) {
+		chatMessageRedisRepository.cacheMember(memberCacheDto);
 	}
 }
