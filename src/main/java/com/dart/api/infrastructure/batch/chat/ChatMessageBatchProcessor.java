30fee15eeb6e3afe21193ecc6a6df3907bf6c9b4
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-06-26 19:09:53 +0900

    feat: Processor 프로세스 구현

---
+++ src/main/java/com/dart/api/infrastructure/batch/chat/ChatMessageBatchProcessor.java
@@ -0,0 +1,46 @@
+package com.dart.api.infrastructure.batch.chat;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.dart.api.domain.chat.entity.ChatMessage;
+import com.dart.api.domain.chat.entity.ChatRoom;
+import com.dart.api.domain.chat.repository.ChatMessageRedisRepository;
+import com.dart.api.domain.member.entity.Member;
+import com.dart.api.dto.chat.request.ChatMessageCreateDto;
+import com.dart.api.dto.chat.request.cache.ChatRoomCacheDto;
+import com.dart.api.dto.chat.request.cache.MemberCacheDto;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ChatMessageBatchProcessor implements ItemProcessor<ChatMessageCreateDto, ChatMessage> {
+
+	private final EntityManager entityManager;
+	private final ChatMessageRedisRepository chatMessageRedisRepository;
+
+	@Value("#{jobParameters['chatRoomId']}")
+	private Long chatRoomId;
+
+	@Override
+	public ChatMessage process(ChatMessageCreateDto chatMessageCreateDto) {
+		final ChatRoom chatRoom = getChatRoomFromCache(chatRoomId);
+		final Member member = getMemberFromCache(chatMessageCreateDto.sender());
+		return ChatMessage.chatMessageFromCreateDto(chatRoom, member, chatMessageCreateDto);
+	}
+
+	private ChatRoom getChatRoomFromCache(Long chatRoomId) {
+		final ChatRoomCacheDto chatRoomCacheDto = chatMessageRedisRepository.getChatRoomCache(chatRoomId);
+		return entityManager.getReference(ChatRoom.class, chatRoomCacheDto.chatRoomId());
+	}
+
+	private Member getMemberFromCache(String nickname) {
+		final MemberCacheDto memberCacheDto = chatMessageRedisRepository.getMemberCache(nickname);
+		return entityManager.getReference(Member.class, memberCacheDto.memberId());
+	}
+}
