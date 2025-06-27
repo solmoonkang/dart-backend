7ec16e4002d885d2d4c2ab116f81714491971be0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-06-27 13:16:25 +0900

    refactor: 채팅메시지 생성 및 조회 시 로직 메서드 분리

--- src/main/java/com/dart/api/application/chat/message/ChatMessageService.java
+++ src/main/java/com/dart/api/application/chat/message/ChatMessageService.java
@@ -1,18 +1,10 @@
 package com.dart.api.application.chat.message;
 
-import static com.dart.global.common.util.RedisConstant.*;
-import static com.dart.global.error.model.ErrorCode.*;
-
-import java.util.List;
-import java.util.Set;
-
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import com.dart.api.application.chat.message.batch.ChatMessageBatchService;
-import com.dart.api.domain.chat.repository.ChatMessageRedisRepository;
 import com.dart.api.dto.chat.request.ChatMessageCreateDto;
-import com.dart.global.error.exception.NotFoundException;
+import com.dart.api.dto.chat.response.ChatMessageReadDto;
+import com.dart.api.dto.page.PageResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,39 +12,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ChatMessageService {
 
-	private final ChatMessageBatchService chatMessageBatchService;
-	private final ChatMessageRedisRepository chatMessageRedisRepository;
+	private final ChatMessageReadService chatMessageReadService;
+	private final ChatMessageWriteService chatMessageWriteService;
 
 	public void saveChatMessage(Long chatRoomId, ChatMessageCreateDto chatMessageCreateDto) {
-		validateChatRoomExistsInCache(chatRoomId);
-		validateMemberExistsInCache(chatMessageCreateDto.sender());
-		chatMessageRedisRepository.saveChatMessage(chatRoomId, chatMessageCreateDto);
+		chatMessageReadService.validateChatRoomExistsInCache(chatRoomId);
+		chatMessageReadService.validateMemberExistsInCache(chatMessageCreateDto.sender());
+		chatMessageWriteService.saveChatMessage(chatRoomId, chatMessageCreateDto);
 	}
 
-	@Scheduled(cron = "0 */10 * * * *")
-	public void runBatchForAllRooms() {
-		final long maxScore = System.currentTimeMillis();
-
-		final Set<String> chatRoomKeys = chatMessageRedisRepository.findAllChatRoomKeysWithMessages();
-		final List<Long> chatRoomIds = chatRoomKeys.stream()
-			.map(key -> key.replace(REDIS_CHAT_MESSAGE_PREFIX, ""))
-			.map(Long::parseLong)
-			.toList();
-
-		for (Long chatRoomId : chatRoomIds) {
-			chatMessageBatchService.executeForChatRoom(chatRoomId, maxScore);
-		}
-	}
-
-	public void validateChatRoomExistsInCache(Long chatRoomId) {
-		if (!chatMessageRedisRepository.isChatRoomCached(chatRoomId)) {
-			throw new NotFoundException(FAIL_CHAT_ROOM_NOT_FOUND);
-		}
-	}
-
-	public void validateMemberExistsInCache(String nickname) {
-		if (!chatMessageRedisRepository.isMemberCached(nickname)) {
-			throw new NotFoundException(FAIL_MEMBER_NOT_FOUND);
-		}
+	public PageResponse<ChatMessageReadDto> getAllChatMessages(Long chatRoomId, int page, int size) {
+		return chatMessageReadService.findChatMessages(chatRoomId, page, size);
 	}
 }
