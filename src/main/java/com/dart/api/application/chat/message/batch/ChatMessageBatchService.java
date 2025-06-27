6fab94aa84a1c61ad41c68d9e0533ae5085869ff
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-06-27 13:15:39 +0900

    refactor: 채팅 메시지 배치 처리 로직 메서드 분리

--- src/main/java/com/dart/api/application/chat/message/batch/ChatMessageBatchService.java
+++ src/main/java/com/dart/api/application/chat/message/batch/ChatMessageBatchService.java
@@ -1,12 +1,19 @@
 package com.dart.api.application.chat.message.batch;
 
+import static com.dart.global.common.util.RedisConstant.*;
+
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import com.dart.api.application.chat.message.ChatMessageReadService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,18 +25,36 @@ public class ChatMessageBatchService {
 
 	private final JobLauncher jobLauncher;
 	private final Job chatMessageBatchJob;
+	private final ChatMessageReadService chatMessageReadService;
 
-	public void executeForChatRoom(Long chatRoomId, long maxScore) {
-		JobParameters jobParameters = new JobParametersBuilder()
+	@Scheduled(cron = "0 */10 * * * *")
+	public void runBatchForAllRooms() {
+		final long maxScore = System.currentTimeMillis();
+
+		final Set<String> chatRoomKeys = chatMessageReadService.findAllChatRoomKeysWithMessages();
+		final List<Long> chatRoomIds = chatRoomKeys.stream()
+			.map(key -> key.replace(REDIS_CHAT_MESSAGE_PREFIX, ""))
+			.map(Long::parseLong)
+			.toList();
+
+		for (Long chatRoomId : chatRoomIds) {
+			executeBatch(buildJobParameters(chatRoomId, maxScore));
+		}
+	}
+
+	private void executeBatch(JobParameters jobParameters) {
+		try {
+			jobLauncher.run(chatMessageBatchJob, jobParameters);
+		} catch (Exception e) {
+			log.error("[✅ LOGGER] 채팅방 배치 실패: {}", jobParameters, e);
+		}
+	}
+
+	private JobParameters buildJobParameters(Long chatRoomId, long maxScore) {
+		return new JobParametersBuilder()
 			.addLong("chatRoomId", chatRoomId)
 			.addLong("maxScore", maxScore)
 			.addString("UUID", UUID.randomUUID().toString())
 			.toJobParameters();
-
-		try {
-			jobLauncher.run(chatMessageBatchJob, jobParameters);
-		} catch (Exception e) {
-			log.error("[✅ LOGGER] {}번 채팅방 배치에 실패했습니다.", chatRoomId, e);
-		}
 	}
 }
