90bd0c2371cf6c923e59c1eb9fa2e8bc230837fc
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-06-27 13:14:26 +0900

    feat: 부족한 채팅 메시지 RDB 조회 로직 구현

--- src/main/java/com/dart/api/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/dart/api/domain/chat/repository/ChatMessageRepository.java
@@ -1,6 +1,11 @@
 package com.dart.api.domain.chat.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.dart.api.domain.chat.entity.ChatMessage;
@@ -8,6 +13,9 @@ import com.dart.api.domain.chat.entity.ChatRoom;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+
+	@Query("SELECT m FROM ChatMessage m WHERE m.chatRoom.id = :chatRoomId ORDER BY m.createdAt DESC")
+	List<ChatMessage> findMessagesByChatRoomWithOffset(@Param("chatRoomId") Long chatRoomId, Pageable pageable);
 
 	void deleteByChatRoom(ChatRoom chatRoom);
 }
