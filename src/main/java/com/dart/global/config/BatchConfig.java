8060af49fc26af01c65229e3c0bd5a103edb04e7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-06-30 01:48:23 +0900

    refactor: StepScope 적용

--- src/main/java/com/dart/global/config/BatchConfig.java
+++ src/main/java/com/dart/global/config/BatchConfig.java
@@ -2,21 +2,26 @@ package com.dart.global.config;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import com.dart.api.domain.chat.entity.ChatMessage;
+import com.dart.api.domain.chat.repository.ChatMessageRedisRepository;
 import com.dart.api.dto.chat.request.ChatMessageCreateDto;
-import com.dart.api.infrastructure.batch.chat.ChatMessageBatchProcessor;
 import com.dart.api.infrastructure.batch.chat.ChatMessageRedisReader;
-import com.dart.api.infrastructure.batch.chat.ChatMessageWriter;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class BatchConfig {
@@ -24,22 +29,35 @@ public class BatchConfig {
 	private static final String CHAT_MESSAGE_BATCH_JOB_NAME = "chatMessageBatchJob";
 	private static final String CHAT_MESSAGE_STEP_NAME = "chatMessageStep";
 
-	private final ChatMessageRedisReader chatMessageRedisReader;
-	private final ChatMessageBatchProcessor chatMessageBatchProcessor;
-	private final ChatMessageWriter chatMessageWriter;
+	private final ChatMessageRedisRepository chatMessageRedisRepository;
 
 	@Bean
-	public Job chatMessageBatchJob(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+	@StepScope
+	public ChatMessageRedisReader chatMessageRedisReader(
+		@Value("#{jobParameters['chatRoomId']}") Long chatRoomId,
+		@Value("#{jobParameters['maxScore']}") Long maxScore) {
+
+		log.info("✅ Reader Bean 생성됨: chatRoomId={}, maxScore={}", chatRoomId, maxScore);
+		return new ChatMessageRedisReader(chatMessageRedisRepository, chatRoomId, maxScore);
+	}
+
+	@Bean
+	public Job chatMessageBatchJob(JobRepository jobRepository, Step chatMessageStep) {
 		return new JobBuilder(CHAT_MESSAGE_BATCH_JOB_NAME, jobRepository)
-			.start(chatMessageStep(jobRepository, platformTransactionManager))
+			.start(chatMessageStep)
 			.build();
 	}
 
 	@Bean
-	public Step chatMessageStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+	public Step chatMessageStep(
+		JobRepository jobRepository,
+		PlatformTransactionManager platformTransactionManager,
+		ItemProcessor<ChatMessageCreateDto, ChatMessage> chatMessageBatchProcessor,
+		ItemWriter<ChatMessage> chatMessageWriter) {
+
 		return new StepBuilder(CHAT_MESSAGE_STEP_NAME, jobRepository)
 			.<ChatMessageCreateDto, ChatMessage>chunk(100, platformTransactionManager)
-			.reader(chatMessageRedisReader)
+			.reader(chatMessageRedisReader(null, null))
 			.processor(chatMessageBatchProcessor)
 			.writer(chatMessageWriter)
 			.build();
