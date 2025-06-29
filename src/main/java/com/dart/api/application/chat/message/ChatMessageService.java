1aaec3c1c884522e7bebef274a58de9875bd89ee
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-06-30 01:44:55 +0900

    refactor: 채팅 메시지 저장 로직 매개변수 수정

--- src/main/java/com/dart/api/application/chat/message/ChatMessageService.java
+++ src/main/java/com/dart/api/application/chat/message/ChatMessageService.java
@@ -2,7 +2,7 @@ package com.dart.api.application.chat.message;
 
 import org.springframework.stereotype.Service;
 
-import com.dart.api.dto.chat.request.ChatMessageCreateDto;
+import com.dart.api.dto.chat.request.ChatMessageSendDto;
 import com.dart.api.dto.chat.response.ChatMessageReadDto;
 import com.dart.api.dto.page.PageResponse;
 
@@ -15,10 +15,10 @@ public class ChatMessageService {
 	private final ChatMessageReadService chatMessageReadService;
 	private final ChatMessageWriteService chatMessageWriteService;
 
-	public void saveChatMessage(Long chatRoomId, ChatMessageCreateDto chatMessageCreateDto) {
+	public void saveChatMessage(Long chatRoomId, ChatMessageSendDto chatMessageSendDto) {
 		chatMessageReadService.validateChatRoomExistsInCache(chatRoomId);
-		chatMessageReadService.validateMemberExistsInCache(chatMessageCreateDto.sender());
-		chatMessageWriteService.saveChatMessage(chatRoomId, chatMessageCreateDto);
+		chatMessageReadService.validateMemberExistsInCache(chatMessageSendDto.sender());
+		chatMessageWriteService.saveChatMessage(chatRoomId, chatMessageSendDto);
 	}
 
 	public PageResponse<ChatMessageReadDto> getAllChatMessages(Long chatRoomId, int page, int size) {
