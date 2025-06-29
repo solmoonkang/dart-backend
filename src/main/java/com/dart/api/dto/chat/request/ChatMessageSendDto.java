84b540eaaa104bb69f311eb26da06c70c8346c9b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-06-30 01:43:15 +0900

    refactor: Valid 어노테이션 추가

--- src/main/java/com/dart/api/dto/chat/request/ChatMessageSendDto.java
+++ src/main/java/com/dart/api/dto/chat/request/ChatMessageSendDto.java
@@ -1,21 +1,20 @@
 package com.dart.api.dto.chat.request;
 
-import java.time.LocalDateTime;
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record ChatMessageSendDto(
-	Long memberId,
+	@NotBlank(message = "[❎ ERROR] 사용자 닉네임을 입력해주세요.")
+	String sender,
 
-	Long chatRoomId,
-
+	@NotBlank(message = "[❎ ERROR] 메시지 내용을 입력해주세요.")
+	@Size(max = 50, message = "[❎ ERROR] 메시지 내용은 50자 이내여야 합니다.")
 	String content,
-
-	LocalDateTime createdAt,
 
 	boolean isAuthor,
 
-	long expirySeconds
+	String profileImageUrl
 ) {
 }
